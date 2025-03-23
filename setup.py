e663f082f81d7f1c451a2f7ffa8b7d2ea988604c
mrupp <mrupprecht@wavenet.at>
2025-03-23 18:33:31 +0100

    packaged project

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "agg_dim",
+    version = 0.1,
+    packages = find_packages(),
+    licence_files = ("LICENSE"),
+    install_requires = ["branca>=0.7.2",
+                        "certifi>=2024.8.30",
+                        "charset-normalizer>=2.0.4",
+                        "contourpy>=1.2.1",
+                        "cycler>=0.12.1",
+                        "folium>=0.17.0",
+                        "fonttools>=4.51.0",
+                        "h5py>=3.11.0",
+                        "idna>=3.4",
+                        "Jinja2>=3.1.3",
+                        "kiwisolver>=1.4.5",
+                        "llvmlite>=0.42.0",
+                        "MarkupSafe>=2.1.3",
+                        "matplotlib>=3.6.0",
+                        "numba>=0.59.0",
+                        "numpy>=1.26.4",
+                        "packaging>=23.2",
+                        "pillow>=10.3.0",
+                        "pyparsing>=3.1.2",
+                        "python-dateutil>=2.8.2",
+                        "requests>=2.31.0",
+                        "six>=1.16.0",
+                        "urllib3>=2.0.7",
+                        "xyzservices>=2022.9.0",
+                        ]
+    )
