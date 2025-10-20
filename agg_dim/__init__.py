463492a07c714f333aa4d1ef44aebadc108f517a
Matthäus Rupprecht <166562578+matrup01@users.noreply.github.com>
2025-10-20 09:14:40 +0200

    Update __init__.py

--- agg_dim/__init__.py
+++ agg_dim/__init__.py
@@ -1,6 +1,6 @@
 from .drone import Dronedata, DroneWrapper
 from .fluoreszenz import FData, NewFData
 from .lowcostsensors import CCS811, SEN55, FlyingFlo_USB
-from .pops import Pops
+from .particle_counters import Pops, OPC
 from .wibs import WIBS
 from .weather import WeatherData
